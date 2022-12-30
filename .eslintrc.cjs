76c9d27dcf3e80df35ea817c1d5b673cb660478a
Sebastian Peters <koelnconcert@googlemail.com>
2022-12-30 12:48:44 +0100

    configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,30 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/standard'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/html-closing-bracket-spacing': [
+      'warn',
+      {
+        // prefer <foobar/> over <foobar />. This is compliant with
+        // https://vuejs.org/style-guide/rules-strongly-recommended.html#self-closing-components
+        selfClosingTag: 'never'
+      }
+    ],
+    /*
+     * add vue3 uncategorized
+     */
+    'vue/no-v-text': 'warn',
+    'vue/no-useless-v-bind': 'warn',
+    'vue/no-useless-mustaches': 'warn',
+    'vue/no-empty-component-block': 'warn',
+    'vue/no-unused-refs': 'warn',
+    'vue/no-unused-properties': 'warn'
   }
 }
