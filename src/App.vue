d0d55ce846bf24b257376f6ca32865d6a9aeb78f
Sebastian Peters <koelnconcert@googlemail.com>
2022-12-31 10:46:44 +0100

    layout

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,14 @@ import MicrophoneControl from '@/components/MicrophoneControl.vue'
 </script>
 
 <template>
-  <main class="p-2">
-    <MicrophoneControl/>
-    <DebugVoices/>
-  </main>
+  <div class="grid grid-cols-[1fr_auto] grid-rows-[1fr_auto] h-screen w-screen">
+    <main class="p-2 overflow-scroll">
+      <DebugVoices/>
+    </main>
+    <aside class="row-span-2 w-64 h-full right-0 bg-zinc-800 p-2">
+      <MicrophoneControl/>
+    </aside>
+    <footer class="w-full h-10 bg-zinc-800">
+    </footer>
+  </div>
 </template>
