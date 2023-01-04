cd3f1550cb84fe90eb25a94b03f1a7abb5d0417c
Sebastian Peters <koelnconcert@googlemail.com>
2023-01-04 17:52:12 +0100

    extract GenericButtonForDialog

---
+++ src/components/GenericButtonForDialog.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+import GenericDialog from './GenericDialog.vue'
+const show = ref(false)
+</script>
+
+<template>
+  <button
+    class="flex items-center"
+    @click="show = true"
+  >
+    <slot name="button"/>
+  </button>
+  <GenericDialog v-model:open="show">
+    <slot name="content"/>
+  </GenericDialog>
+</template>
--- src/components/OptionButtons.vue
+++ src/components/OptionButtons.vue
@@ -1,21 +1,17 @@
 <script setup>
-import { ref } from 'vue'
-import ModalDialog from '@/components/ModalDialog.vue'
 import DebugVoices from '@/components/DebugVoices.vue'
 import { AdjustmentsHorizontalIcon } from '@heroicons/vue/24/solid'
-
-const showDebugVoices = ref(false)
+import GenericButtonForDialog from './GenericButtonForDialog.vue'
 </script>
 
 <template>
-  <button
-    class="flex items-center"
-    @click="showDebugVoices = true"
-  >
-    <AdjustmentsHorizontalIcon class="w-6 h-6 mr-1"/>
-    Options
-  </button>
-  <ModalDialog v-model:open="showDebugVoices">
-    <DebugVoices/>
-  </ModalDialog>
+  <GenericButtonForDialog>
+    <template #button>
+      <AdjustmentsHorizontalIcon class="w-6 h-6 mr-1"/>
+      Voices
+    </template>
+    <template #content>
+      <DebugVoices/>
+    </template>
+  </GenericButtonForDialog>
 </template>
