1968ad066adae2ba3b4ec03b6bfc5197463a6fb5
Sebastian Peters <koelnconcert@googlemail.com>
2025-10-04 23:09:10 +0200

    adjust model conversion script

--- server/convert-hf-model-to-whisper.py
+++ server/convert-hf-model-to-whisper.py
@@ -1,8 +1,13 @@
 #!/bin/env python3
-#https://github.com/openai/whisper/discussions/830#discussioncomment-10026678
+#
+# Refined whisper model on hugging face often (always?/normally?) have different layer/states names
+# This script converts a given model to a model that is compliant with the original whisper code
+# 
+# Original script from https://github.com/openai/whisper/discussions/830#discussioncomment-10026678
 import whisper
 import re
 import torch
+import sys
 
 def hf_to_whisper_states(text):
     text = re.sub('.layers.', '.blocks.', text)
@@ -27,18 +32,23 @@ def hf_to_whisper_states(text):
     text = re.sub('proj_out.weight', 'decoder.token_embedding.weight', text)
     return text
 
-# Load HF Model
-hf_state_dict = torch.load("whisper-medium-id.bin", map_location=torch.device('cpu'))    # pytorch_model.bin file
+hf_model_filename = sys.argv[1]
+whisper_model_name = sys.argv[2]
+output_model_filename = sys.argv[3]
 
-# Rename layers
+print("loading hugging face model", hf_model_filename)
+hf_state_dict = torch.load(hf_model_filename, map_location=torch.device('cpu'))    # pytorch_model.bin file
+
+print("renaming layers")
 for key in list(hf_state_dict.keys())[:]:
     new_key = hf_to_whisper_states(key)
     hf_state_dict[new_key] = hf_state_dict.pop(key)
 
-model = whisper.load_model('medium')
-dims = model.dims
-# Save it
+print("loading whisper model", whisper_model_name)
+model = whisper.load_model(whisper_model_name)
+
+print("saving new model", output_model_filename)
 torch.save({
     "dims": model.dims.__dict__,
     "model_state_dict": hf_state_dict
-}, "whisper-model.bin")
+}, output_model_filename)
